b8e3c2fca3815b4cb983d13a62f1c1729b93e1bd
Jeff Forester <jeff.forester@gmail.com>
2026-09-11 22:18:18 +0000

    Complete data validation step

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -84,6 +84,13 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert np.isfinite(kl_div) and kl_div < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame) -> None:
+    """Test that the dataset contains a reasonable number of rows."""
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float) -> None:
+    """Test that all prices are within the expected range."""
+    idx = data['price'].between(min_price, max_price)
+
+    assert np.sum(~idx) == 0
